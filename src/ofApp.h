b128541ce1192e003e9ba6e9812e163dd8fff12e
jason van cleave <jvcleave@gmail.com>
2017-11-27 04:39:57 +0000

    Panel class

--- src/ofApp.h
+++ src/ofApp.h
@@ -2,134 +2,159 @@
 
 #include "ofMain.h"
 
+class Panel
+{
+public:
+    int width;
+    int height;
+    ofPixels pixels;
+    ofImage image;
+    unsigned char* bytes;
+    int numPixels;
+    int circleSize = 5;
+    float pitch = circleSize*2;
+    
+    int drawWidth;
+    int drawHeight;
+    Panel()
+    {
+        width = 24;
+        height = 16;
+        numPixels = width*height;
+        
+        bytes  = new unsigned char[numPixels];
+        
+        drawWidth =  width*pitch;
+        drawHeight = height*pitch;
+
+    }
+    void setup(string imagePath)
+    {
+        image.load(imagePath);
+        image.resize(width, height);
+        image.update();
+        pixels = image.getPixels();
+        
+    }
+    void draw()
+    {
+        ofPushMatrix();
+        ofPushStyle();
+        int counter = 0;
+        ofColor foregroundColor;
+        
+        for(int x=0; x<width; x++)
+        {
+            for(int y=0; y<height; y++)
+            {
+                
+                ofColor color = pixels.getColor(x, y);
+                if(color == ofColor::white)
+                {
+                    bytes[counter] = '0';
+                    foregroundColor = ofColor::white;
+                    
+                    
+                }else
+                {
+                    bytes[counter] = '1'; 
+                    foregroundColor = ofColor::black; 
+                    
+                } 
+                
+                
+                ofSetRectMode(OF_RECTMODE_CORNER); //set rectangle mode to the corner
+                ofSetColor(foregroundColor);
+                ofDrawCircle(x*pitch, y*pitch, circleSize);
+                drawWidth = x*pitch;
+                drawHeight = y*pitch;
+                //ofDrawBitmapStringHighlight(info.str(), x*30, y*30, ofColor::black, foregroundColor);
+                
+                counter++;
+            }
+        }
+        ofPopStyle();
+        ofPopMatrix();
+        //ofLog() << "drawWidth: " << drawWidth << " drawHeight: " << drawHeight;
+
+    }
+    
+};
+
 class ofApp : public ofBaseApp{
     
 public:
 
-    ofImage image;
-    vector<int> panel;
-    int panelWidth = 24;
-    int panelHeight = 16;
+
 
     ofSerial    serial;
-    unsigned char* bytes;
-    
     string outputString;
-    int            nBytesRead = 0;                    // how much did we read?
     bool didSend = false;
-    //--------------------------------------------------------------
-    void setup(){
+    
+    vector<Panel> panels;
+    void setup()
+    {
         //ofSetVerticalSync(false);
         
         ofSetLogLevel(OF_LOG_VERBOSE);
         //ofSetFrameRate(2);
-        bytes  = new unsigned char[panelWidth*panelHeight];
+        
         serial.listDevices();
         vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
 
         int baud = 115200;
         serial.setup("/dev/ttyACM0", baud); //open the first device
         
-        for(int i=0; i<panelWidth*panelHeight; i++)
-        {
-            if(i%2 == 0)
-            {
-                panel.push_back(0);
-            }else
-            {
-                
-                panel.push_back(1);
-            }
-            
-        }
         
-        image.load("of.png");
-        image.resize(panelWidth, panelHeight);
-        image.update();
-        ofLog() << image.getWidth() << " : " << image.getHeight();
+        Panel panel;
+        panel.setup("of.png");
+        
+        Panel panel2;
+        panel2.setup("of.png");
+        
+        panels.push_back(panel);
+        panels.push_back(panel2);
 
-        /*
-        for(int x=0; x<image.getWidth(); x++)
-        {
-            for(int y=0; y<image.getHeight(); y++)
-            {
-                int position = x*y;
-                ofColor color = ofColor::red;
-                
-                if(position%2 == 0)
-                {
-                    
-                    image.setColor(position, ofColor::red);
-                    
-                }
-                
-                
-            }
-        }
-        image.update();*/
-        ofLog() << "panel.size: " << panel.size();
+    }
+    
+    void update()
+    {
         
     }
     
-    //--------------------------------------------------------------
-    void update(){
-        
-    }
-    
-    //--------------------------------------------------------------
-    void draw(){
-
+    void draw()
+    {
+        int spacing = 60;
         ofPushMatrix();
-        ofPushStyle();
-        int position = 0;
-        int counter = 0;
-        ofColor foregroundColor;
         ofTranslate(60, 60);
-        int circleSize = 5;
-        float pitch = circleSize*2;
-        ofPixels pixels = image.getPixels();
-
-        for(int x=0; x<image.getWidth(); x++)
+        for(size_t i=0; i<panels.size(); i++)
         {
-            for(int y=0; y<image.getHeight(); y++)
-            {
+            ofPushMatrix();
+            ofTranslate(panels[i].drawWidth*i, 0);
+            panels[i].draw();
+            ofPopMatrix();
 
-                ofColor color = pixels.getColor(x, y);
-                if(color == ofColor::white)
-                {
-                    bytes[counter] = '0';
-                    foregroundColor = ofColor::white;
-
-                    
-                }else
-                {
-                    bytes[counter] = '1'; 
-                    foregroundColor = ofColor::black; 
-
-                } 
-
-                
-                ofSetRectMode(OF_RECTMODE_CORNER); //set rectangle mode to the corner
-                ofSetColor(foregroundColor);
-                ofDrawCircle(x*pitch, y*pitch, circleSize);
-                
-                //ofDrawBitmapStringHighlight(info.str(), x*30, y*30, ofColor::black, foregroundColor);
-
-                counter++;
-            }
         }
-        ofPopStyle();
         ofPopMatrix();
+
+        
         //ofDrawBitmapStringHighlight(outputString, 100, 100);
         if(!didSend)
         {
             ofSleepMillis(2000);
 
-            int written = serial.writeBytes(bytes, 384);
-            if(written == 384)
+            for(size_t i=0; i<panels.size(); i++)
             {
-                didSend = true;
+                Panel panel = panels[i];
+                int written = serial.writeBytes(panel.bytes, panel.numPixels);
+                if(written == panel.numPixels)
+                {
+                    didSend = true;
+                }
+                
             }
+            
+            
             
         }
         //ofLog() << "didSend: " << didSend;
