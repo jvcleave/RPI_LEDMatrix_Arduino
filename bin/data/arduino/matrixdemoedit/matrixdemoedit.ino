af4ae1a385a27214d2d7eb930b41f0bbd9ed973b
jason van cleave <jvcleave@gmail.com>
2017-11-28 06:32:34 +0000

    magic forumlas

--- bin/data/arduino/matrixdemoedit/matrixdemoedit.ino
+++ bin/data/arduino/matrixdemoedit/matrixdemoedit.ino
@@ -15,8 +15,6 @@ class Panel
 
     uint8_t startX = 0;
     uint8_t endX = 24;
-    uint8_t startY = 0;
-    uint8_t endY = 16;
     Panel()
     {
       
@@ -31,32 +29,60 @@ class Panel
     }
 };
 
-Vector<Panel*> panels = Vector<Panel*>();
-Vector<int8_t> pins = Vector<int8_t>();
-char* buffer;
-int bufferSize;
+Panel* panels;
+int8_t* pins;
+#define numPanels 3
 
-bool doDrawDebug = false;
-String debugString = "";
+
+
+#define bufferSize (48*numPanels)
+
 bool hasData = false;
+char buffer[bufferSize];
+
+
+#define numValues (384*numPanels)
+
+uint8_t values[numValues];
+
+
 
 void setup() 
 {
+  pins = new int8_t[numPanels];
+  
+  for (int8_t i=0; i< numPanels; i++) 
+  {
+        pins[i] = i+4;
+  }
 
-  for (int8_t i=4; i< 12; i++) 
+  panels = new Panel[numPanels];
+  
+  for (size_t i=0; i< numPanels; i++) 
   {
-        pins.push_back(i);
-      
+      Panel panel;
+      panel.setup(i, pins[i]);
+      panels[i] = panel;
   }
-  for (size_t i=0; i< 2; i++) 
-  {
-      Panel* panel = new Panel();
-      panel->setup(i, pins[i]);
-      panels.push_back(panel);
-  }
-    bufferSize = 384*panels.size();
-    buffer = new char [bufferSize];
-    matrix = new Adafruit_HT1632LEDMatrix(HT_DATA, HT_WR, pins);
+    
+
+    
+        
+
+    int valueCounter  = 0;
+    bool isSet = false;
+    for(int i=0; i<bufferSize; i++)
+    {
+      for(int j=0; j<8; j++)
+      {
+        values[valueCounter] = 1;
+        valueCounter++;
+      }
+    }
+    //hasData = true;
+
+    
+    matrix = new Adafruit_HT1632LEDMatrix(HT_DATA, HT_WR, pins, numPanels);
     Serial.setTimeout(10000);
     Serial.begin(115200);
     matrix->begin(ADA_HT1632_COMMON_16NMOS);
@@ -64,100 +90,123 @@ void setup()
     delay(500);
     matrix->clearScreen();
     matrix->setTextWrap(false);
+    
+    
+    
 }
 
 
-void drawDebug()
-{
-      debugString = "";
-      matrix->clearScreen();
-      matrix->setCursor(0, 0);
-      debugString.concat(String(panels.size()));
-      debugString.concat(" bufferSize:");
-      debugString.concat(String(bufferSize));
-      matrix->print(debugString);
-      matrix->writeScreen();
-}
+
+
 void loop() 
 {
 
   bool isFrameNew = false;
   String incomingString = "INCOMING: ";
-  if (Serial.available()) 
+  if(!hasData)
   {
- 
-    size_t inByte = 0;
-    inByte = Serial.readBytes(buffer, bufferSize);
+    if (Serial.available()) 
+    {
     
-    incomingString = String(inByte);
-
-    if(inByte == bufferSize)
-    {
-      isFrameNew = true;
-      hasData = true;
-    }
-
-  }else
-  {
-    if(!hasData)
-    {
-        drawDebug(); 
-        return;  
-    }
- 
-  }
-
-if(isFrameNew)
-  {
-    matrix->clearScreen();
-    matrix->setCursor(0, 0);
-   
-   size_t counter = 0;
-
-    for (size_t i=0; i< panels.size(); i++) 
-    {
-
-        Panel* panel = panels[i];
-        
-        for (uint8_t x=panel->startX; x<panel->endX; x++) 
-        {
-          for (uint8_t y=0; y< 16; y++) 
+      size_t inByte = 0;
+      inByte = Serial.readBytes(buffer, bufferSize);
+      
+      incomingString = String(inByte);
+  
+      if(inByte == bufferSize)
+      {
+          int bitCounter = 0;
+          for(size_t i=0; i<bufferSize; i++)
           {
-            //String charString = String(buffer[counter]);
-            //String zerostring = String(0);
-            if(buffer[counter]== '0')
+            for (size_t n = 0; n < 8; n++) 
             {
-              matrix->clrPixel(x, y);
-            }else
-            {
-              matrix->setPixel(x, y);
-            }
-            counter++;
-          
+                int currentBit = (buffer[i] >> n) & 1U;
+                int isBitOn = (currentBit == 0);
+                values[bitCounter] = isBitOn;
+                bitCounter++;
 
+            }
+          }
+
+        isFrameNew = true;
+        hasData = true;
+      }
+    }
+ }
+  if(isFrameNew)
+    {
+      matrix->clearScreen();
+      matrix->setCursor(0, 0);
+     
+     size_t counter = 0;
+  
+      for (size_t i=0; i< numPanels; i++) 
+      {
+  
+          Panel* panel = &panels[i];
+          
+          for (uint8_t x=panel->startX; x<panel->endX; x++) 
+          {
+            for (uint8_t y=0; y< 16; y++) 
+            {
+              if(values[counter]== 0)
+              {
+                matrix->clrPixel(x, y);
+              }else
+              {
+                matrix->setPixel(x, y);
+              }
+              counter++;
+              
+            }
+          }
+          
+      }
+      matrix->writeScreen();
+
+    }
+}
+
+#if 0
+void valueLoop()
+{
+  int pixelCounter = 0;
+
+  while(1)
+  {
+      matrix->clearScreen();
+      matrix->setCursor(0, 0);
+      //matrix->drawGrayscaleBitmap(0, 0, (uint8_t*)values, matrix->width(), matrix->height());
+
+      for (uint8_t x=0; x<matrix->width(); x++) 
+      {
+        for (uint8_t y=0; y< matrix->height(); y++) 
+        {
+          if(values[pixelCounter] == 1)
+          {
+            matrix->setPixel(x, y);
+          }else
+          {
+              matrix->clrPixel(x, y);
+          }
+          
+          
+          matrix->writeScreen();
+          if(pixelCounter+1 < valueCounter)
+          {
+              pixelCounter++;
+          }else
+          {
+            pixelCounter = 0;  
           }
         }
-        
-    }
-    matrix->writeScreen();
-
-       /*
-        * delay(1000);
-     
-      size_t numWritten = Serial.write("NEW");
-      incomingString.concat(":");
-      incomingString.concat(String(numWritten));
-      matrix->setCursor(24, 0);
-
-      matrix->print(incomingString);
-      matrix->writeScreen();*/
-
+      }
+      delay(1000);
   }
-
-
-  //delay(50);
-
+   
 }
+
+
 
 void demoLoop()
 {
@@ -210,3 +259,30 @@ void demoLoop()
       
     }  
 }
+
+void drawDebug()
+{
+      String debugString = "";
+      //delay(2000);
+
+      matrix->clearScreen();
+      matrix->setCursor(0, 0);
+      
+      //debugString.concat(String(panels.size()));
+     // debugString.concat(" bufferSize:");
+      //debugString.concat(String(bufferSize));
+
+      debugString.concat("hD:");
+      debugString.concat(String(hasData)); 
+      
+      debugString.concat("vC:");
+      debugString.concat(String(valueCounter));
+      
+      
+      debugString.concat("bSz:");
+      debugString.concat(String(bufferSize));
+      
+      matrix->print(debugString);
+      matrix->writeScreen();
+}
+#endif
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,27 +9,38 @@ public:
     int height;
     ofPixels pixels;
     ofImage image;
-    vector<unsigned char> bytes;
+    vector<unsigned char>payload;
+    
     int numPixels;
     float circleSize = 3;
     float pitch = circleSize*2;
-    
+    bitset<384> bits;
     int drawWidth;
     int drawHeight;
-    bool doRandom = true;
-
+    bool doRandom;
+    bool doRows;
+    
     Panel()
     {
         width = 24;
         height = 16;
         numPixels = width*height;
         doRandom = false;
-        bytes.resize(numPixels);
+        doRows = false;
+        //bits.resize(numPixels);
         
         drawWidth =  width*pitch;
         drawHeight = height*pitch;
-
+        payload.resize(48);
+        
+        
     }
+    
+    void setValue(int currentCharIndex, int index, int value )
+    {
+        
+    }
+    
     void setup(string imagePath)
     {
         image.load(imagePath);
@@ -37,45 +48,74 @@ public:
         image.update();
         pixels = image.getPixels();
         ofColor foregroundColor;
-        int counter = 0;
-
+        int pixelCounter = 0;
+        int currentCharIndex = 0;
+        int bitIndex = 0;
         for(int x=0; x<width; x++)
         {
             for(int y=0; y<height; y++)
             {
+                //ofLog() << " pixelCounter : " << pixelCounter << " currentCharIndex: " << currentCharIndex << " bitIndex: " << bitIndex;
+                unsigned char* currentChar = &payload[currentCharIndex];
+                unsigned long newbit;
+                bool isOn = false;
+                
                 if(doRandom)
                 {
                     int rNum = (int)ofRandom(0, 56);
+                    
                     if(rNum%2 == 0)
+                    {                        
+                        isOn = true;
+                    }
+                    
+                    
+                }
+                if(doRows)
+                {
+                    if(x%2 == 0)
                     {
-                        bytes[counter] = '0';
+                        isOn = true;
                         
                     }
-                    else
-                    {
-                        bytes[counter] = '1'; 
-                        
-                    } 
                     
-                }else
+                }
+                if(!doRows && !doRandom)
                 {
                     ofColor color = pixels.getColor(x, y);
                     if(color == ofColor::white)
                     {
-                        bytes[counter] = '0';
-  
-                    }else
-                    {
-                        bytes[counter] = '1'; 
+                        isOn = true; 
                         
-                    }  
+                    } 
                     
                 }
+                if(isOn)
+                {
+                    newbit = !!1;
+                }else
+                {
+                    newbit = !!0;
+                }
+                *currentChar ^= (-newbit ^ *currentChar) & (1UL << bitIndex);
+                //leds.push_back(isOn);
+                
                 drawWidth = x*pitch;
                 drawHeight = y*pitch;
-                counter++;
+                pixelCounter++;
+                if(pixelCounter % 8 == 0)
+                {
+                    currentCharIndex++;
+                    bitIndex = 0;
+                }else
+                {
+                    bitIndex++;
+                    
+                }
+                
             }
         }
+        
     }
     void draw()
     {
@@ -83,26 +123,37 @@ public:
         ofPushStyle();
         int counter = 0;
         ofColor color;
+        vector<bool>leds;
+        for(size_t i=0; i<payload.size(); i++)
+        {
+            for (size_t n = 0; n < 8; n++) 
+            {
+                int bit = (payload[i] >> n) & 1U;
+                bool isBitOn = (bit == 0);
+                leds.push_back(isBitOn);
+                
+            }
+        }
         for(int x=0; x<width; x++)
         {
             for(int y=0; y<height; y++)
             {
-    
-                if(bytes[counter] == '0')
+                
+                if(leds[counter] == 0)
                 {
                     color = ofColor::white;
-
+                    
                     
                 }else
                 {
                     color = ofColor::black; 
-
+                    
                 }
                 
                 ofSetRectMode(OF_RECTMODE_CORNER); //set rectangle mode to the corner
                 ofSetColor(color);
                 ofDrawCircle(x*pitch, y*pitch, circleSize);
-
+                
                 //ofDrawBitmapStringHighlight(info.str(), x*30, y*30, ofColor::black, foregroundColor);
                 
                 counter++;
@@ -110,8 +161,7 @@ public:
         }
         ofPopStyle();
         ofPopMatrix();
-        //ofLog() << "drawWidth: " << drawWidth << " drawHeight: " << drawHeight;
-
+        
     }
     
 };
@@ -119,9 +169,9 @@ public:
 class ofApp : public ofBaseApp{
     
 public:
-
-
-
+    
+    
+    
     ofSerial    serial;
     string outputString;
     bool didSend = false;
@@ -136,21 +186,22 @@ public:
         
         serial.listDevices();
         vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
-
+        
         int baud = 115200;
-        serial.setup("/dev/ttyACM0", baud); //open the first device
+        serial.setup("/dev/cu.usbmodem3A21", baud); //open the first device
         
         for(size_t i=0; i<8; i++)
         {
             Panel panel;
             if(i%2 ==0)
             {
-                panel.doRandom = true; 
+                //panel.doRows = true; 
             }
+            //panel.doRows = true;
             panel.setup("of.png");
             panels.push_back(panel);
         }
-
+        
     }
     
     void update()
@@ -169,10 +220,10 @@ public:
             ofTranslate(panels[i].drawWidth*i, 0);
             panels[i].draw();
             ofPopMatrix();
-
+            
         }
         ofPopMatrix();
-
+        
         
         //ofDrawBitmapStringHighlight(outputString, 100, 100);
         if(!didSend)
@@ -180,13 +231,13 @@ public:
             ofSleepMillis(2000);
             vector<unsigned char>payload;
             
-            for(size_t i=0; i<2; i++)
+            for(size_t i=0; i<3; i++)
             {
                 Panel panel = panels[i];
                 
-                for(size_t n=0; n<panel.bytes.size(); n++)
+                for(size_t n=0; n<panel.payload.size(); n++)
                 {
-                    payload.push_back(panel.bytes[n]);
+                    payload.push_back(panel.payload[n]);
                 }
                 
                 
@@ -198,62 +249,14 @@ public:
             {
                 didSend = true;
                 ofLog() << "didSend: " << didSend;
-                //serial.flush();    
                 
             }
             
-#if 0
-            if(didSend)
-            {
-                
-                int bytes_required = 3;  
-                unsigned char bytes[bytes_required];  
-                int bytes_remaining = bytes_required;  
-                while ( bytes_remaining > 0 )  
-                {  
-                    int avail = serial.available();
-                    //ofSleepMillis(3000);
-                    while(serial.available() > 0)
-                    {
-                        // check for data  
-                        // try to read - note offset into the bytes[] array, this is so that we   
-                        // don't overwrite the bytes we already have  
-                        int result = serial.readBytes( &bytes[bytes_required-bytes_remaining], bytes_remaining ); 
-                        
-                        ofLog() << "avail :" << avail << " result: " << result;
-
-                        // check for error code  
-                        if ( result == OF_SERIAL_ERROR )  
-                        {  
-                            ofLog() << "unrecoverable error reading serial" ;  
-                            break;  
-                        }  
-                        else if ( result == OF_SERIAL_NO_DATA )  
-                        {  
-                            //ofLog() << "OF_SERIAL_NO_DATA" ;  
-                            
-                        }  
-                        else  
-                        {  
-                            // we read some data!  
-                            // maybe not all..  
-                            bytes_remaining -= result;  
-                            ofLog() << "bytes_remaining: " << bytes_remaining <<  " result: " << result; 
-                        } 
-                        
-                    }
-                    
-                    
-                    
-                    //ofSleepMillis(1000);
-                    
-                }  
-                
-            }
-#endif
+            
             
         }
     }
-
+    
 };
+
 
